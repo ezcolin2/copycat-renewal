6c7ea59797aa6793d224a768f8dd78fbd22c2fc1
growth <growth_s@naver.com>
2024-06-11 07:30:05 +0900

    [Feat] passport, json 연결

--- client/src/App/index.jsx
+++ client/src/App/index.jsx
@@ -1,11 +1,23 @@
-import {useState} from 'react';
+import { useState } from "react";
 import logo from "./logo.svg";
 import { Body } from "./styles.jsx";
-import LogInAndJoin from '../pages/LogInAndJoin';
+import LogInAndJoin from "../pages/LogInAndJoin";
+import axios from "axios";
 function App() {
-  return <Body>
-    <LogInAndJoin></LogInAndJoin>
-  </Body>;
+  axios
+    .post("http://localhost:3001/api/v1/users/join", {
+      nickname: "hello",
+      password: "el",
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => console.log(error));
+  return (
+    <Body>
+      <LogInAndJoin></LogInAndJoin>
+    </Body>
+  );
 }
 
 export default App;
